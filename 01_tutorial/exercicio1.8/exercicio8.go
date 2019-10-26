f39eb26209d653142cf37b7c393bf8ee06b62be6
alexandreps1123 <alexandre.ps1123@gmail.com>
2019-10-26 14:29:15 -0300

    exercicios fecth 8 e 9

---
+++ 01_tutorial/exercicio1.8/exercicio8.go
@@ -0,0 +1,31 @@
+//fetch: modificar para que o prefixo http:// seja acrescentado a cada URL
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		//strings.HasPrefix("a", "b") verifica se a string 'a' tem como prefixo a string 'b', e retorna um bool
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
---
+++ 01_tutorial/exercicio1.9/exercicio9.go
@@ -0,0 +1,29 @@
+//modificar fetch para que exiba tambem o codigo de status HTTP
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//imprime o codigo de status http
+		fmt.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
