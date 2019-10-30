8310538b864996f20cb373c1c706e44f9e703925
MCTIC <alexandre.ps1123@gmail.com>
2019-10-30 08:50:10 -0300

    exemplos de declaracao de variavel e uso de ponteiro

---
+++ 02_estrutura_do_programa/variaveis.exemplos/variaveis.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i, j, k int                 //declarando multiplas variaveis do mesmo tipo
+	var b, f, s = true, 2.3, "four" //declaracao de diversas variaveis de diferentes tipos
+	a := "olá!"                     //declaracao curta de variavel
+	i, j = 10, 20
+	i, j = j, i //swap
+	fmt.Println(i, j, k, b, f, s, a)
+
+	//ponteiros
+	x := 1
+	p := &x           //p recebe o endereco de x
+	*p++              //incrementa o valor de x
+	fmt.Println(x, p) //imprime o valor de x e o endereco
+	incr(&x)
+	fmt.Println(incr(&x), *p, x)
+	/*
+		for count := 0; count <= 10; count++ {
+			fmt.Println(count)
+		}
+	*/
+	//funcao new(T): cria uma variavel sem nome do tipo T e retorna seu endereço
+	c := new(int)
+	fmt.Println(*c)
+	*c = 2
+	fmt.Println(*c)
+	teste := newInt()
+	fmt.Println(*teste, teste)
+}
+
+func incr(p *int) int {
+	*p++ //incrmenta o valor para o qual p aponta; nao altera p
+	return *p
+}
+
+//cria uma variavel inteira e retorna seu endereco
+func newInt() *int {
+	return new(int)
+}
